7e247eff45142465ec02613611b869a2868bef25
Sakshi-hp <legendsakshi005@gmail.com>
2025-07-11 02:28:20 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,10 @@
+a= int(input('Enter Ist Number='))
+b= int(input('Enter 2nd Number='))
+s=a+b
+p=a*b
+d=a-b
+div=a/b
+print('Sum=',s)
+print('Product=',p)
+print('Divisor=',div)
+print('Subtraction=',d)
---
+++ saki.py
@@ -0,0 +1,4 @@
+a= str(input('Enter first name'))
+b= str(input('Enter second name'))
+s=a+b
+print('Hello , ' +s+ ' Welcome to the Python Program')
